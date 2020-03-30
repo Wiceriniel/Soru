9067686eb5f2ce005131b6c61ea9e002a962cef6
Wiceriniel <wiceriniel@gmail.com>
2020-03-30 21:33:50 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const imagecategories = require('./config/imagecategories.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if ((msg.channel.parentID === imagecategories.TYT || msg.channel.parentID === imagecategories.AYT) && msg.author.id != client.user.id && msg.attachments.size === 0) {
+    msg.guild.channels.get(imagecategories.logchannel).send(`<#${msg.channel.id}> <@${msg.author.id}> : ${msg.cleanContent}`);
+    msg.delete();
+  }
+  
+});
